0dbde78f81e6d265cb3f5fc8c5e9f996fdafe625
Rodrigo Cabello <rodrigo.cabello.malagon@outlook.es>
2018-01-03 11:44:32 +0100

    Change scripts name environment

--- scripts/environment.bat
+++ scripts/environment.bat
@@ -4,5 +4,5 @@ if %errorlevel% neq 0 (
     echo "Hubo un error creando el entorno virtual" 
     exit /b %ERRORLEVEL%
     ) else (
-        activate cntk22dotnetmalaga
+        activate cntk22mnist
     )
--- scripts/environment.sh
+++ scripts/environment.sh
@@ -4,5 +4,5 @@ if [ $? -ne 0 ]
 then
     echo 'Hubo un error creando el entorno virtual';
 else
-    source activate cntk22dotnetmalaga
+    source activate cntk22mnist
 fi
